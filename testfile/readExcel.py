861d362a0f318c97061eceb0c78b9024c6042e7e
likedi421847541 <421847541@qq.com>
2020-08-25 16:18:50 +0800

    Revert "2020/08/24"
    This reverts commit 75ae068e9a927ab6b21fe98c34b4883d54550435.

--- testfile/readExcel.py
+++ testfile/readExcel.py
@@ -1,29 +1,21 @@
 # -*- coding:utf-8 -*-
 import os
-from testFile import getPathInfo  # 自己定义的内部类，该类返回项目的绝对路径
-# 调用读Excel的第三方库xlrd
-from xlrd import open_workbook
+from testfile import getpathInfo
+from xlrd import open_workbook  # 调用读 Excel 的第三方库 xlrd
+path = getpathInfo.get_Path()  # 拿到该项目的绝对地址
 
-# 拿到该项目所在的绝对路径
-path = getPathInfo.get_Path()
-
-
-class readExcel():
-    def get_xls(self, xls_name, sheet_name):  # xls_name填写用例的Excel名称 sheet_name该Excel的sheet名称
-        cls = []
-        # 获取用例文件路径
-        xlsPath = os.path.join(path,  xls_name)
-        file = open_workbook(xlsPath)  # 打开用例Excel
-        sheet = file.sheet_by_name(sheet_name)  # 获得打开Excel的sheet
-        # 获取这个sheet内容行数
-        nrows = sheet.nrows
-        for i in range(nrows):  # 根据行数做循环
-            if sheet.row_values(i)[0] != u'case_name':  # 如果这个Excel的这个sheet的第i行的第一列不等于case_name那么我们把这行的数据添加到cls[]
+class  readExcel():
+    def get_xls(self,xls_name,sheet_name):  # xls_name 填写用例的 Excel 名称，sheet_name 该 Excel 的 sheet 名称
+        cls =[]
+        xlspath = os.path.join(path,xls_name)
+        file = open_workbook(xlspath) # 打开用例 Excel
+        sheet = file.sheet_by_name(sheet_name)  #  获得打开 Excel 的 sheet
+        nrows = sheet.nrows # 获取这个 sheet 内容行数
+        for i in range(nrows): #根据行数做循环
+            if sheet.row_values(i)[0] != u'case_name': #如果这个 Excel 的这个 sheet 的第 i行的第一列不等于 case_name，那么我们把这行数据添加到 cls[]
                 cls.append(sheet.row_values(i))
         return cls
-
-
-if __name__ == '__main__':  # 我们执行该文件测试一下是否可以正确获取Excel中的值
-    print(readExcel().get_xls('login_case.xlsx', 'login'))
-    print(readExcel().get_xls('login_case.xlsx', 'login')[0][1])
-    print(readExcel().get_xls('login_case.xlsx', 'login')[1][2])
+if __name__ == '__main__':
+    print(readExcel().get_xls('testFile.xlsx','login'))
+    print(readExcel().get_xls('testFile.xlsx','login')[0][1])
+    print(readExcel().get_xls('testFile.xlsx','login')[1][2])
--- testfile/test_api.py
+++ testfile/test_api.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 import flask
 import json
 from flask import request
